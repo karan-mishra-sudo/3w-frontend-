43bb54e84b010540f4086c39be43d79d37222667
karan-mishra-sudo <mishrazack69@gmail.com>
2025-01-20 21:30:17 +0530

    Update UserForm.js

--- src/components/User/UserForm.js
+++ src/components/User/UserForm.js
@@ -14,7 +14,7 @@ export default function UserForm() {
                     <button className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150" type="submit">Submit</button>
                 </form>
             </div>
-            <a href="/admin" className='text-cyan-300 font-semibold m-3' target='_blanck'>Login in Adimn Account</a>
+            <a href="/admin" className='text-cyan-300 font-semibold m-3' target='_blanck'>Login in Admin Account</a>
         </div>
 
     )
